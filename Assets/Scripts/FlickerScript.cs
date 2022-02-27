6fd0fe9832b99f3b4f858153a19009e1cec791df
Jeffrey Fulmer Gardner <jeffreyfulmergardner@Jeffreys-MBP.attlocal.net>
2022-02-26 23:02:49 -0800

    adding title Scene

---
+++ Assets/Scripts/FlickerScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlickerScript : MonoBehaviour
+{
+    public Text startText;
+    private int counter =0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(counter==60)
+        {
+            startText.enabled = false;
+        }
+        if(counter==120)
+        {
+            startText.enabled = true;
+            counter = 0;
+        }
+        counter++;
+    }
+}
