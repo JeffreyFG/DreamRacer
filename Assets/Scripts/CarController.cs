a280e0c0150a64f582c12052f6c6fddbf1549190
Alex Shirazi <alexshirazi@Alexs-Mac-mini.local>
2022-04-15 01:19:28 -0700

    bug where transmission shifts 50+ gears

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -3,11 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-
+using System.Timers;
+using System.Threading.Tasks;
 
     [System.Serializable] public struct EngineSpecs {
   //more specs to be added later
+    public float idle;
+    public float lowerPowerBand;
+    public float upperPowerBand;
     public float redline;
+    public double gearShiftTimeSeconds;
 }
 public class CarController : MonoBehaviour
 {
@@ -30,14 +35,12 @@ public class CarController : MonoBehaviour
     private float currentSteerAngle;
     private float currentbrakeForce;
     private bool isBraking;
+    private float[,] engineTorque = new float[4,2];
 
-
-
-
+    [SerializeField] private float maxMotorForce;
     [SerializeField] private float motorForce;
     [SerializeField] private float brakeForce;
     [SerializeField] private float maxSteerAngle;
-
 
 
     [SerializeField] private WheelCollider frontLeftWheelCollider;
@@ -50,25 +53,64 @@ public class CarController : MonoBehaviour
     [SerializeField] private Transform rearLeftWheelTransform;
     [SerializeField] private Transform rearRightWheelTransform;
 
+
+    void Start(){
+        EngineSetup();
+    }
+
+    private void EngineSetup(){
+    engineTorque[0,0] = EngineSpecs.idle;
+    engineTorque[1,0] = EngineSpecs.lowerPowerBand;
+    engineTorque[2,0] = EngineSpecs.upperPowerBand;
+    engineTorque[3,0] = EngineSpecs.redline;
+
+    engineTorque[0,1] = powerSetup(EngineSpecs.idle);
+    engineTorque[1,1] = powerSetup(EngineSpecs.lowerPowerBand);
+    engineTorque[2,1] = powerSetup(EngineSpecs.upperPowerBand);
+    engineTorque[3,1] = powerSetup(EngineSpecs.redline);
+
+    }
+    
+    private float powerSetup(float power){
+        //power * constant / rpm
+        return (power*7000)/currentRPM;
+    }
+    
+
+
     private void FixedUpdate()
     {
-        speedMPH=RB.velocity.magnitude*3.6f;
+        
           
-        if(speedMPH==30){
-        }
+        handleMotorForce();
         GetInput();
         HandleMotor();
         HandleSteering();
         UpdateWheels();
         HandleEngine();
-        manager.StartInteraction();   
         GearShift();
-    }
+        manager.StartInteraction();   
 
+    }
+private void handleMotorForce(){
+
+    if(currentRPM<EngineSpecs.lowerPowerBand){
+        motorForce=maxMotorForce*.25f;
+    }
+    if(currentRPM>EngineSpecs.lowerPowerBand&&currentRPM<EngineSpecs.upperPowerBand){
+        motorForce=maxMotorForce;
+    }
+    if(currentRPM>EngineSpecs.upperPowerBand&&currentRPM<EngineSpecs.redline){
+        motorForce=maxMotorForce*.75f;
+    }
+}
 private void HandleEngine(){
      //MAKE SURE the number in GEARRATIO[number] is equal to the number of elements in UNITY EDITOR OTHERWISE GEARBOX WONT WORK!!!!!! for example gearRatio[5], editor must have elements 0-5!!!!
+if(currentRPM<EngineSpecs.redline){
         currentRPM=(RB.velocity.magnitude*600/(0.29f*2*Mathf.PI))*gearRatio[5]*gearRatio[currentGear];
-    
+       speedMPH=RB.velocity.magnitude*3.6f;
+    }
+
         //Debug.Log(currentRPM+" RPM");
        
 }
@@ -96,14 +138,19 @@ private void HandleEngine(){
         
     }
     private void GearShift(){
-        if(currentGear==gearRatio.Length&&currentRPM==EngineSpecs.redline){
-            motorForce=0f;
-        }
-        if(verticalInput==1&&currentRPM>EngineSpecs.redline){
-            currentGear++;
+        if(verticalInput==1&&currentRPM>EngineSpecs.redline &&currentGear<6){
+        //     verticalInput=0;
+        //    Debug.Log("Before Delay");
+        //    await Task.Delay((int)(EngineSpecs.gearShiftTimeSeconds*1000));
+        //    motorForce=1000f;
+        //     currentGear++;
+        //    Debug.Log("After Delay");
+        //     verticalInput=1;
+        currentGear++;
+
 
         }
-          if(verticalInput==-1&&currentGear>0&&currentRPM<minRPM){
+          if(currentGear>1&&currentRPM<minRPM){
             currentGear--;
         }
         speedMPH = GetComponent<Rigidbody>().velocity.magnitude*3.6f;
