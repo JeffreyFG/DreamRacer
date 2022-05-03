2e17001d72783ebe449e63d3da5df3d36d130798
Edwin Menjivar <edwinm.csm1ke@gmail.com>
2022-05-03 10:56:07 -0700

    Working on powerups

---
+++ Assets/Scripts/GamePlayScripts/PowerUps/SpeedUp.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUp : PowerUpEffect
+{
+    public override void Apply(GameObject target)
+    {
+
+    }
+}
